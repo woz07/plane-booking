d03844585135d70391e30d7e339c550b9105bcb8
Muhammad Ravat <m0ravat736@gmail.com>
2024-10-11 13:32:18 +0100

    added nav bar

---
+++ client/src/Components/TopNav.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import plane from "/public/plane.svg";
+import { useState } from "react";
+function TopNav(){
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+}
+  return (
+    <nav className="fixed top-0 left-0 w-full h-20 flex items-center justify-between px-4 z-10 max-[900px]:justify-normal bg-white  dark:bg-black">
+      <ul className='float-left'>
+        <li><button onClick={toggleSidebar} className="flex text-black dark:text-white items-center gap-1 font-bold text-standard round"><img src={plane} alt="Logo" className='h-16 dark:bg-white text-white rounded-2xl  p-1 ' />Plane Booking Prototype</button></li>
+      </ul>
+      <ul className="flex space-x-[30%] text-standard max-[650px]:hidden text-black dark:text-white">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/flights">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to ="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+      <ul className="flex space-x-[10%] max-[900px]:absolute right-0 pr-4">
+      </ul>
+
+      {isSidebarOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 text-white flex flex-col items-center justify-center z-20">
+          <button onClick={toggleSidebar} className="absolute top-5 right-5 text-3xl">
+            &times;
+          </button>
+          <ul className="space-y-8 text-3xl">
+            <img src="" alt="Logo" className="bg-slate-600 rounded-full" />
+            <img src={plane} alt="Logo" className="bg-slate-600 rounded-full" />
+            <li><Link to="/" onClick={toggleSidebar}>Home</Link></li>
+            <li><Link to="/flights" onClick={toggleSidebar}>Projects</Link></li>
+            <li><Link to="/contact" onClick={toggleSidebar}>Contact</Link></li>
+            <li><Link to="/login" onClick={toggleSidebar}>Login</Link></li>
+            <li><Link to="/signup" onClick={toggleSidebar}>Signup</Link></li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
+export default TopNav;
