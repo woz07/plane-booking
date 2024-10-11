d94ca9a87e2680310abb03d4e15f91670b7a4b7c
Muhammad Ravat <m0ravat736@gmail.com>
2024-10-11 16:40:33 +0100

    implemented login/signup and flight pages

--- client/src/Pages/Flights/flights.jsx
+++ client/src/Pages/Flights/flights.jsx
@@ -1,9 +1,35 @@
 import TopNav from "../../Components/TopNav";
 function Flights(){
+    const flights = [
+        {destination:"Kenya", date:new Date(2025, 9, 27),from:"Heathrow",capacity:"45",price:"$43"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+
+        {destination:"London", date:new Date(2024, 11, 13),from:"Luton",capacity:"20",price:"$6542"},
+    ]
     return(
-        <div className="pt-32 text-center">
+        <div className="pt-32 text-standard bg-slate-300 text-orange-800 font-bold w-full h-[100%] px-10 ">
             <TopNav />
-            <h1>Flights</h1>
+            <h1 className="text-center text-large mb-10">Flights</h1>
+            <div className="width-[80%] text-center flex flex-wrap justify-start gap-x-10 mx-auto my-auto">
+            {flights.map((flight, index) => (
+                <div key={index} className="w-72 p-4 bg-black text-white mb-8">
+                    <h1 className="border-b-2 border-white mb-4">
+                        {flight.from} to {flight.destination}
+                    </h1>
+                    <p>
+                        Date: {flight.date.toDateString()} <br />
+                        Price: {flight.price} <br />
+                        Capacity: {flight.capacity}
+                    </p>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
--- client/src/Pages/Login/login.jsx
+++ client/src/Pages/Login/login.jsx
@@ -1,9 +1,11 @@
 import TopNav from "../../Components/TopNav";
 function Login(){
     return(
-        <div className="pt-32 text-center">
+        <div className="pt-32 text-standard bg-slate-300 text-orange-800 font-bold w-full h-screen">
             <TopNav />
-            <h1>Login</h1>
+            <div className="w-[60%] min-h-80 bg-black text-white mx-auto my-auto mt-4 text-center p-4">
+                Login
+            </div>
         </div>
     )
 }
