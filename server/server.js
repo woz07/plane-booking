10ebd4f76bca7c6094fc682c81c44d06e5c03f0d
woz07 <muhsameer9@gmail.com>
2024-10-11 17:26:47 +0100

    add structure and better comments

--- server/server.js
+++ server/server.js
@@ -27,7 +27,12 @@ server.get('/', (request, response) => {
   response.status(200).json({ error: false, message: 'Hello from the server!' });
 });
 
-server.post('/login', (request, response) => {
+/**
+ * This allows the user to sign into their acccount
+ * @String username = The username the user wantts to login with
+ * @String password = The password the user wants to login with
+ */
+server.post('/user/login', (request, response) => {
   const { username, password } = request.body;
   if (username == null || username.trim() == '' || password == null || password.trim() == '') {
     response.status(400).json({ error: true, message: 'The username or password was incorrect' });
@@ -48,12 +53,17 @@ server.post('/login', (request, response) => {
     response.status(200).json({ 
       errors: false, 
       message: 'There was no issue in fetching the data',
-      result: results
+      result: true
     });
   })
 });
 
-server.post('/signup', (request, response) => {
+/**
+ * This allows the user to create an account
+ * @String username = The username the user wants
+ * @String password = The password the user wants
+ */
+server.post('/user/signup', (request, response) => {
   const { username, password } = request.body;
   if (username == null || username.trim() == '' || password == null || password.trim() == '') {
     response.status(400).json({ error: true, message: 'The username or password is not allowed' });
@@ -75,7 +85,68 @@ server.post('/signup', (request, response) => {
   })
 });
 
+/**
+ * This returns all the flights
+ */
+server.post('/flights/all', (request, response) => {
 
+});
+
+/**
+ * This returns all the flights from a specific airline
+ * @String airline = The airline to check for
+ */
+server.post('/flights/airline', (request, response) => {
+  
+});
+
+/**
+ * This returns all the flights to a specific country
+ * @String country = The country to check for
+ */
+server.post('/flights/to', (request, response) => {
+
+});
+
+/**
+ * This returns all the flights from a specific country
+ * @String country = The country to check for
+ */
+server.post('/flights/from', (request, response) => {
+
+});
+
+/**
+ * This returns all the flights that have that many availabl slots
+ * @Int slots = The amount of available slots there should be
+ */
+server.post('/flights/availableslots', (request, response) => {
+
+});
+
+/**
+ * This returns all the flights departing at the requested date
+ * @Date date = The date to query for
+ */
+server.post('/flights/depart', (request, response) => {
+
+});
+
+/**
+ * This returns all the flights arriving at the requested date
+ * @Date date = The date to query for
+ */
+server.post('/flights/arrival', (request, response) => {
+
+});
+
+/**
+ * This returns all the bookings of the user based on the username
+ * @String username = The username to find all bookings for
+ */
+server.post('/bookings/by', (request, response) => {
+
+});
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
